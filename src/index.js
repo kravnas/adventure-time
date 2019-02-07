57877636552f81aae60fc874ca9826c9758324cd
kravnas <kjetil.ravnas@soprasteria.com>
2019-02-07 12:56:10 +0100

    Changes to structure

--- src/index.js
+++ src/index.js
@@ -6,7 +6,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+  }
+  
+  const user = {
+    firstName: 'Kjetil',
+    lastName: 'Ravnås'
+  };
+  
+  const element = (
+    <h1>
+      Hello, {formatName(user)}!
+    </h1>
+  );
   
   ReactDOM.render(
     element,
